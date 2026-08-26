c7f69c95ed9f730abaa242c21d6c0d23d55a0400
alvaroporras2002-coder <alvaro.porras2002@gmail.com>
2026-08-26 11:27:46 -0600

    Add Firebase configuration template

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,14 @@
+/*
+  Firebase configuration.
+  Later, replace each value with your own Firebase Web App values.
+  Do NOT put passwords, private keys, or service-account files here.
+*/
+
+window.FIREBASE_CONFIG = {
+  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
+  authDomain: "REPLACE_WITH_PROJECT_ID.firebaseapp.com",
+  projectId: "REPLACE_WITH_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "REPLACE_WITH_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_APP_ID"
+};
